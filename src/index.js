9ee58eb46bfd34ce21ed959088bb53b0f4d45793
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2023-03-26 21:05:58 +0530

    Added Support Page with Footer

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Support from './support';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Support />
   </React.StrictMode>
 );
 
